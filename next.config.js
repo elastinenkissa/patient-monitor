6a05380f91c88e66aa8f41adc0b1e17f0b58800b
Nusret Wazowski <maxiegothfemboi@gmail.com>
2023-04-25 18:01:45 +0200

    Changed nextjs config to fix dynamic styling issues with framer motion

--- next.config.js
+++ next.config.js
@@ -3,7 +3,30 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [{ hostname: 'th.bing.com' }]
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      const splitChunks = config.optimization && config.optimization.splitChunks;
+      if (splitChunks) {
+        const cacheGroups = splitChunks.cacheGroups;
+        if (cacheGroups) {
+          const existingStylesCachegroup = cacheGroups.styles;
+          if (existingStylesCachegroup) {
+            existingStylesCachegroup.name = 'styles';
+          } else {
+            cacheGroups.styles = {
+              name: 'styles',
+              test: /\.(css|sass|scss)$/,
+              chunks: 'all',
+              enforce: true
+            };
+          }
+        }
+      }
+    }
+    return config;
   }
+  
 };
 
 module.exports = nextConfig;
