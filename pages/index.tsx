6a5e44e32acbcf9e2f30c47872bf95017c14ac0f
Nusret Wazowski <maxiegothfemboi@gmail.com>
2023-04-28 11:03:12 +0200

    Added embed

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,12 @@ const Welcome: FC = () => {
     <>
       <Head>
         <title>Patient Monitor</title>
-        <meta name="description" content="Patient monitoring and management services avaialble to anyone" />
+        <meta
+          name="description"
+          content="Patient monitoring and management services avaialble to anyone"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:image" content="../public/embed.png" />
       </Head>
     </>
   );
